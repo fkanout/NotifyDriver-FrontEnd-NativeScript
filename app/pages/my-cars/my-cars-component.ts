3d1d3acee730855d716ac21ba92049ea6bd99cdd
Faisal KANOUT <f.kanout@gmail.com>
2017-02-18 23:37:13 +0100

    New design

---
+++ app/pages/my-cars/my-cars-component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by JS on 18/02/2017.
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    templateUrl: 'pages/my-cars/my-cars-component.html',
+
+})
+export class MyCars implements  OnInit {
+    ngOnInit(): void {
+    }
+
+}
